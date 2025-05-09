bb546d28e02d6ff1f523a5857e6654978e28fa80
harshitxrajput <harshitxrajput@gmail.com>
2025-05-09 23:54:42 +0530

    setup shadcn and convex

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 export default function Home() {
     return (
         <div>
             Harshit Rajput
+            <Button>Hey Babyy!</Button>
         </div>
   )
 }
